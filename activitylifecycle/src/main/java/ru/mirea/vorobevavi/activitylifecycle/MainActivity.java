754412b956a7490203e89676f07db23f7088f0af
cheletobred <vicka.vorobjeva2017@yandex.ru>
2024-03-24 16:21:52 +0300

    practice redone

--- activitylifecycle/src/main/java/ru/mirea/vorobevavi/activitylifecycle/MainActivity.java
+++ activitylifecycle/src/main/java/ru/mirea/vorobevavi/activitylifecycle/MainActivity.java
@@ -13,8 +13,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart()");
+
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Activity перезапущена");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Activity активна");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "Activity на паузе");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "Activity остановлена");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "Activity уничтожена");
     }
 }
